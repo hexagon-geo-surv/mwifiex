83dac3172e9e2ca0c1704f39c899f7352bfc098d
Andrey Zhizhikin <andrey.zhizhikin@leica-geosystems.com>
2023-08-31 08:22:56 +0200

    moal_cfg80211: add link parameter to TDLS management
    Since upstream commit c6112046b1a9 ("wifi: cfg80211: make TDLS
    management link-aware"), TDLS management API has additional link_id
    parameter which is missing in the implementation.
    
    Add this parameter to woal_cfg80211_tdls_mgmt() and make it dependent on
    kernel version (>= 6.5.y).
    
    Signed-off-by: Andrey Zhizhikin <andrey.zhizhikin@leica-geosystems.com>

--- mxm_wifiex/wlan_src/mlinux/moal_sta_cfg80211.c
+++ mxm_wifiex/wlan_src/mlinux/moal_sta_cfg80211.c
@@ -211,6 +211,9 @@ int woal_cfg80211_tdls_mgmt(struct wiphy *wiphy, struct net_device *dev,
 #else
 			    u8 *peer,
 #endif
+#if CFG80211_VERSION_CODE >= KERNEL_VERSION(6, 5, 0)
+			    int link_id,
+#endif
 			    u8 action_code, u8 dialog_token, u16 status_code,
 #if CFG80211_VERSION_CODE >= KERNEL_VERSION(3, 15, 0)
 			    u32 peer_capability,
@@ -7477,9 +7480,15 @@ void woal_check_auto_tdls(struct wiphy *wiphy, struct net_device *dev)
 	if (tdls_discovery)
 #if CFG80211_VERSION_CODE >= KERNEL_VERSION(3, 15, 0)
 #if CFG80211_VERSION_CODE >= KERNEL_VERSION(3, 17, 0)
+#if CFG80211_VERSION_CODE >= KERNEL_VERSION(6, 5, 0)
+		woal_cfg80211_tdls_mgmt(wiphy, dev, bcast_addr, 0,
+					TDLS_DISCOVERY_REQUEST, 1, 0, 0, 0,
+					NULL, 0);
+#else
 		woal_cfg80211_tdls_mgmt(wiphy, dev, bcast_addr,
 					TDLS_DISCOVERY_REQUEST, 1, 0, 0, 0,
 					NULL, 0);
+#endif
 #else
 		woal_cfg80211_tdls_mgmt(wiphy, dev, bcast_addr,
 					TDLS_DISCOVERY_REQUEST, 1, 0, 0, NULL,
@@ -8215,6 +8224,13 @@ fail:
 	return ret;
 }
 
+#if CFG80211_VERSION_CODE >= KERNEL_VERSION(6, 5, 0)
+int woal_cfg80211_tdls_mgmt(struct wiphy *wiphy, struct net_device *dev,
+                            const u8 *peer, int link_id, u8 action_code,
+                            u8 dialog_token, u16 status_code,
+                            u32 peer_capability, bool initiator,
+                            const u8 *extra_ies, size_t extra_ies_len)
+#else
 #if CFG80211_VERSION_CODE >= KERNEL_VERSION(3, 17, 0)
 /**
  * @brief Tx TDLS packet
@@ -8282,6 +8298,7 @@ int woal_cfg80211_tdls_mgmt(struct wiphy *wiphy, struct net_device *dev,
 			    t_u8 *peer, u8 action_code, t_u8 dialog_token,
 			    t_u16 status_code, const t_u8 *extra_ies,
 			    size_t extra_ies_len)
+#endif
 #endif
 #endif
 {
