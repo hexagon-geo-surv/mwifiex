9aed920680c21f41c3b584ee5b1b0d0b97bad97d
Sven Püschel <s.pueschel@pengutronix.de>
2025-03-18 10:24:36 +0100

    moal_cfg80211: add link_id parameter to get_tx_power
    add link_id parameter to woal_cfg80211_get_tx_power to make the function
    signature compatible with 6.14 kernels. It was introduced in 7a53af85d3bb
    ("wifi: cfg80211: send MLO links tx power info in GET_INTERFACE").
    
    Signed-off-by: Sven Püschel <s.pueschel@pengutronix.de>

--- mlinux/moal_sta_cfg80211.c
+++ mlinux/moal_sta_cfg80211.c
@@ -155,6 +155,9 @@ static int woal_cfg80211_get_tx_power(struct wiphy *wiphy,
 #if CFG80211_VERSION_CODE >= KERNEL_VERSION(3, 8, 0)
 				      struct wireless_dev *wdev,
 #endif
+#if CFG80211_VERSION_CODE >= KERNEL_VERSION(6, 14, 0)
+				      unsigned int link_id,
+#endif
 				      int *dbm);
 
 static int woal_cfg80211_set_tx_power(struct wiphy *wiphy,
@@ -5925,6 +5928,9 @@ static int woal_cfg80211_set_power_mgmt(struct wiphy *wiphy,
 static int woal_cfg80211_get_tx_power(struct wiphy *wiphy,
 #if CFG80211_VERSION_CODE >= KERNEL_VERSION(3, 8, 0)
 				      struct wireless_dev *wdev,
+#endif
+#if CFG80211_VERSION_CODE >= KERNEL_VERSION(6, 14, 0)
+				      unsigned int link_id,
 #endif
 				      int *dbm)
 {
