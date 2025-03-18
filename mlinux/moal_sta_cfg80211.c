fc0338254fc988bb5562a37184ff7db6546ed080
Sven Püschel <s.pueschel@pengutronix.de>
2025-03-18 10:38:58 +0100

    wifi: cfg80211: add net_device parameter to set_monitor_channel
    Add the new net_device paramter to set_monitor_channel, which was
    introduced by 9c4f83092775
    ("wifi: cfg80211: pass net_device to .set_monitor_channel")
    
    Signed-off-by: Sven Püschel <s.pueschel@pengutronix.de>

--- mlinux/moal_sta_cfg80211.c
+++ mlinux/moal_sta_cfg80211.c
@@ -88,6 +88,9 @@ static const u32 cfg80211_akm_suites[] = {
 #ifdef UAP_SUPPORT
 #if CFG80211_VERSION_CODE >= KERNEL_VERSION(3, 8, 0)
 static int woal_cfg80211_set_monitor_channel(struct wiphy *wiphy,
+#if CFG80211_VERSION_CODE >= KERNEL_VERSION(6, 13, 0)
+					     struct net_device *dev,
+#endif
 					     struct cfg80211_chan_def *chandef);
 #endif
 #endif
@@ -620,6 +623,9 @@ static const struct wiphy_coalesce_support coalesce_support = {
 #ifdef UAP_SUPPORT
 #if CFG80211_VERSION_CODE >= KERNEL_VERSION(3, 8, 0)
 static int woal_cfg80211_set_monitor_channel(struct wiphy *wiphy,
+#if CFG80211_VERSION_CODE >= KERNEL_VERSION(6, 13, 0)
+					     struct net_device *dev,
+#endif
 					     struct cfg80211_chan_def *chandef)
 {
 	moal_handle *handle = (moal_handle *)woal_get_wiphy_priv(wiphy);
