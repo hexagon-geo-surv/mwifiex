fb1fee837f180f62df8cd389424a422c5aad3a90
Sven Püschel <s.pueschel@pengutronix.de>
2025-03-18 11:16:42 +0100

    moal_main: remove unused lib80211 header
    Remove seemingly unused header, as it was also removed in 02f220b52670
    ("wifi: ipw2x00/lib80211: move remaining lib80211 into libipw")
    and removing it doesn't cause any compiliation issues.
    
    Signed-off-by: Sven Püschel <s.pueschel@pengutronix.de>

--- mlinux/moal_main.h
+++ mlinux/moal_main.h
@@ -128,7 +128,6 @@ Change log:
 #include "moal_shim.h"
 /* Wireless header */
 #if defined(STA_CFG80211) || defined(UAP_CFG80211)
-#include <net/lib80211.h>
 #include <net/cfg80211.h>
 #include <net/ieee80211_radiotap.h>
 #endif
